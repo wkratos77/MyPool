833107c0ce3a75cd6c18f8ab0bddc1cd4d7cbe79
wkratos77 <159393650+wkratos77@users.noreply.github.com>
2025-07-26 15:55:21 +0100

    Update ft_strlcat.c

--- TestC03/ex05/ft_strlcat.c
+++ TestC03/ex05/ft_strlcat.c
@@ -1,22 +1,32 @@
-#include <unistd.h>
-
+include <unistd.h>
+//#include <stdio.h>
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	int	i;
+	unsigned int	i;
 	unsigned int	j;
+	unsigned int	l;
 
+	l = 0;
+	while (src[l])
+		l++;
 	i = 0;
-	while (dest[i] && i < size)
-	{
+	while (dest[i])
 		i++;
-
+	if (size <= i)
+		return (size + l);
+	j = 0;
+	while (src[j] && j < size - i - 1)
+	{
+		dest[i + j] = src[j];
+		j++;
 	}
-		while (j < size - 1 &&  src[j])
-		{
-			dest[i + j] = src[j];
-			j++
-		}
-
-	dest[i] = '\0';
-	return (i);
+	dest[i + j] = '\0';
+	return (l + i);
 }
+/*int main()
+{
+    char d1[20] = "abc";
+    char s1[] = "def";
+    printf("%d\n",ft_strlcat(d1,s1,20));
+    printf("%s\n",d1);
+}*/
